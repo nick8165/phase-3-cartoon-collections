fa17864627fa447a39f0b52485cdceae064e9532
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-02-08 16:21:18 -0600

    c

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,41 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+index = 1
+  dwarves.map do |b| 
+    puts "#{index}. #{b}"
+  index += 1
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(element)
+  new_array = []
+  element.map do |b|
+    new_string = b.capitalize
+    new_string << "!"
+    new_array.push(new_string)
+  end
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  find = calls.find do |b|
+    b.length > 4
+  end
+  if find == nil
+    false
+  else 
+    true
+  end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if snacks.include?("cheddar") == true
+    "cheddar"
+  elsif snacks.include?("gouda") == true
+    "gouda"
+  elsif snacks.include?("camembert") == true
+    "camambert"
+  else
+    nil
+  end
 end
